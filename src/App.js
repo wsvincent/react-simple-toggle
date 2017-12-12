b1d6c8129d38ea3a23e7ee57560a72cd89f65b24
William Vincent <will@wsvincent.com>
2017-12-12 09:13:10 -0500

    implement simple toggle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    toggle: true
+  };
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.toggle && <p>This should show and hide.</p>}
+        <button onClick={this.toggle}>Show / Hide</button>
       </div>
     );
   }
